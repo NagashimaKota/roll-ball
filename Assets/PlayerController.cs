f0efdfcb459e5c2abdf198bd8ef3c1f510cef229
長島光太 <se.kokuto@gmail.com>
2018-12-25 00:41:02 +0900

    first

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed = 10;
+
+    void FixedUpdate()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        // 同一のGameObjectが持つRigidbodyコンポーネントを取得
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+
+        // rigidbodyのx軸（横）とz軸（奥）に力を加える
+        rigidbody.AddForce(x * speed, x *speed, z * speed);
+    }
+
+
+}
